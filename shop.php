c96f3f6fe65a4dc05fce1a20ad7b74575a11c94d
Dungprj <doibatcong2003@gmail.com>
2024-09-26 13:39:39 +0700

    fix giao dien hien thi img  shop.php

--- shop.php
+++ shop.php
@@ -436,18 +436,18 @@ $sql_list = mysqli_query($con, "SELECT * FROM `tbl_sanpham` ORDER BY `sanpham_id
 								<div class="product-image-wrapper">
 									<div class="single-products">
 										<div class="productinfo text-center">
-											<img src="images/shop/<?php echo $row['sanpham_image'] ?>" alt="" />
+											<img src="./images/shop/<?php echo $row['sanpham_image'] ?>" alt="" />
 											<h2><?php echo number_format($row['sanpham_giakhuyenmai']) . 'đ'; ?></h2>
 											<p><?php echo $row['sanpham_name']; ?></p>
 											<a href="#" class="btn btn-default add-to-cart"><i class="fa fa-shopping-cart"></i>Thêm vào giỏ hàng</a>
 										</div>
-										<div class="product-overlay">
+										<!-- <div class="product-overlay">
 											<div class="overlay-content">
 												<h2><?php echo number_format($row['sanpham_giakhuyenmai']) . 'đ'; ?></h2>
 												<p><?php echo $row['sanpham_name']; ?></p>
 												<a href="#" class="btn btn-default add-to-cart"><i class="fa fa-shopping-cart"></i>Thêm vào giỏ hàng</a>
 											</div>
-										</div>
+										</div> -->
 									</div>
 									<div class="choose">
 										<ul class="nav nav-pills nav-justified">
