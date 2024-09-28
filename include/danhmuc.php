3e05b003b2606d0b40791b585c2393d1fff18d56
quoc2k3 <quocsp2k3@gmail.com>
2024-09-28 13:45:46 +0700

    Danh muc sp

---
+++ include/danhmuc.php
@@ -0,0 +1,62 @@
+<?php
+	if(isset($_GET['id'])){
+		$id = $_GET['id'];
+	}else{
+		$id = '';
+	}
+	$sql_cate = mysqli_query($con,"SELECT * FROM tbl_category,tbl_sanpham WHERE tbl_category.category_id=tbl_sanpham.category_id AND tbl_sanpham.category_id='$id' ORDER BY tbl_sanpham.sanpham_id DESC");	
+	$sql_category = mysqli_query($con,"SELECT * FROM tbl_category,tbl_sanpham WHERE tbl_category.category_id=tbl_sanpham.category_id AND tbl_sanpham.category_id='$id' ORDER BY tbl_sanpham.sanpham_id DESC");		
+
+	$row_title = mysqli_fetch_array($sql_category);
+	$title = $row_title['category_name'];
+	?>
+<!-- top Products -->
+<div class="col-sm-9 padding-right">
+					<div class="features_items"><!--features_items-->
+						<h2 class="title text-center"><?php echo $title ?></h2>
+						
+						<?php
+						while ($row = mysqli_fetch_array($sql_cate)) {
+						?>
+							<div class="col-sm-4">
+								<div class="product-image-wrapper">
+									<div class="single-products">
+										<div class="productinfo text-center">
+											<img src="images/shop/<?php echo $row['sanpham_image'] ?>" alt="" />
+											<h2><?php echo number_format($row['sanpham_giakhuyenmai']) . 'đ'; ?></h2>
+											<p><?php echo $row['sanpham_name']; ?></p>
+											<a href="#" class="btn btn-default add-to-cart"><i class="fa fa-shopping-cart"></i>Thêm vào giỏ hàng</a>
+										</div>
+										<div class="product-overlay">
+											<div class="overlay-content">
+												<h2><?php echo number_format($row['sanpham_giakhuyenmai']) . 'đ'; ?></h2>
+												<p><?php echo $row['sanpham_name']; ?></p>
+												<a href="#" class="btn btn-default add-to-cart"><i class="fa fa-shopping-cart"></i>Thêm vào giỏ hàng</a>
+											</div>
+										</div>
+									</div>
+									<div class="choose">
+										<ul class="nav nav-pills nav-justified">
+											<li><a href=""><i class="fa fa-plus-square"></i>Add to wishlist</a></li>
+											<li><a href=""><i class="fa fa-plus-square"></i>Add to compare</a></li>
+										</ul>
+									</div>
+								</div>
+							</div>
+						<?php
+						}
+						?>
+	
+	
+						
+					</div><!--features_items-->
+                    <ul class="pagination">
+							<li class="active"><a href="">1</a></li>
+							<li><a href="">2</a></li>
+							<li><a href="">3</a></li>
+							<li><a href="">&raquo;</a></li>
+						</ul>
+				</div>
+	<!-- //top products -->
+
+    
